5d2cedcb9710824e8b184cba534923ebd319468d
hallie214 <younh524@newschool.edu>
2022-02-24 05:04:36 -0500

    Project 2 (Part 4)

---
+++ studio/project2(drafts)/p2p4.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+// alert("[whatever-little-message-you-want]");
+
+$("#title1").hover(function(){
+$("#title1").css("color", "grey");
+});
+$("#title1").mouseout(function(){
+$("#title1").css("color", "black");
+});
+
+// alert("[whatever-little-message-you-want]");
+//
+// $("#title1").click(function(){
+// $("#text").show();
+// });
+//
+// $("#[the-id-of-the-item-you-element-to-drag]").draggable();
+//
+// $("#[the-id-of-element-b]").hover(function(){
+// $("#[the-id-of-element-a]").show();
+// });
+//
+// $("#[the-id-of-what-you'll-hover-on]").hover(function(){
+// $("#[the-id-of-the-element-you-want-to-have-different-css]").css("[the-new-property]", "[the-new-value]");
+// });
+//
+// $("#title1").mouseout(function(){
+// $("#title1").css("color", "black");
+// });
+
+})
