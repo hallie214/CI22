95fb98468cfd9e6348cd26e9b3f2d179c0c9ecc7
hallie214 <younh524@newschool.edu>
2022-03-16 19:27:32 -0400

    Project 3 (Part 1)

--- index.js
+++ index.js
@@ -1,10 +1,246 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
-$("#hover1").hover(function(){
-$("#hover1").css("color", "grey");
+// alert("[whatever-little-message-you-want]");
+
+// $("a:link").hover(function(){
+// $("#title2").hide();
+// });
+
+// alert("[whatever-little-message-you-want]");
+
+$("#p2p7").hover(function(){
+$("#p2p7").css("color", "grey");
 });
-$("#hover1").mouseout(function(){
-$("#hover1").css("color", "black");
+$("#p2p7").mouseout(function(){
+$("#p2p7").css("color", "black");
 });
 
-})
+$("#p2p6i5").hover(function(){
+$("#p2p6i5").css("color", "grey");
+});
+$("#p2p6i5").mouseout(function(){
+$("#p2p6i5").css("color", "black");
+});
+
+$("#p2p6i4").hover(function(){
+$("#p2p6i4").css("color", "grey");
+});
+$("#p2p6i4").mouseout(function(){
+$("#p2p6i4").css("color", "black");
+});
+
+$("#p2p6i3").hover(function(){
+$("#p2p6i3").css("color", "grey");
+});
+$("#p2p6i3").mouseout(function(){
+$("#p2p6i3").css("color", "black");
+});
+
+$("#p2p6i2").hover(function(){
+$("#p2p6i2").css("color", "grey");
+});
+$("#p2p6i2").mouseout(function(){
+$("#p2p6i2").css("color", "black");
+});
+
+$("#p2p6i1").hover(function(){
+$("#p2p6i1").css("color", "grey");
+});
+$("#p2p6i1").mouseout(function(){
+$("#p2p6i1").css("color", "black");
+});
+
+$("#p2p5i3").hover(function(){
+$("#p2p5i3").css("color", "grey");
+});
+$("#p2p5i3").mouseout(function(){
+$("#p2p5i3").css("color", "black");
+});
+
+$("#p2p5i2").hover(function(){
+$("#p2p5i2").css("color", "grey");
+});
+$("#p2p5i2").mouseout(function(){
+$("#p2p5i2").css("color", "black");
+});
+
+$("#p2p5i1").hover(function(){
+$("#p2p5i1").css("color", "grey");
+});
+$("#p2p5i1").mouseout(function(){
+$("#p2p5i1").css("color", "black");
+});
+
+$("#p2p4").hover(function(){
+$("#p2p4").css("color", "grey");
+});
+$("#p2p4").mouseout(function(){
+$("#p2p4").css("color", "black");
+});
+
+$("#p2p3").hover(function(){
+$("#p2p3").css("color", "grey");
+});
+$("#p2p3").mouseout(function(){
+$("#p2p3").css("color", "black");
+});
+
+$("#p2p2").hover(function(){
+$("#p2p2").css("color", "grey");
+});
+$("#p2p2").mouseout(function(){
+$("#p2p2").css("color", "black");
+});
+
+$("#p2p1").hover(function(){
+$("#p2p1").css("color", "grey");
+});
+$("#p2p1").mouseout(function(){
+$("#p2p1").css("color", "black");
+});
+
+$("#p1").hover(function(){
+$("#p1").css("color", "grey");
+});
+$("#p1").mouseout(function(){
+$("#p1").css("color", "black");
+});
+
+// alert("[whatever-little-message-you-want]");
+
+$("#e3").hover(function(){
+$("#e3").css("color", "grey");
+});
+$("#e3").mouseout(function(){
+$("#e3").css("color", "black");
+});
+
+$("#e2e5").hover(function(){
+$("#e2e5").css("color", "grey");
+});
+$("#e2e5").mouseout(function(){
+$("#e2e5").css("color", "black");
+});
+
+$("#e2e4").hover(function(){
+$("#e2e4").css("color", "grey");
+});
+$("#e2e4").mouseout(function(){
+$("#e2e4").css("color", "black");
+});
+
+$("#e2e3").hover(function(){
+$("#e2e3").css("color", "grey");
+});
+$("#e2e3").mouseout(function(){
+$("#e2e3").css("color", "black");
+});
+
+$("#e2e2").hover(function(){
+$("#e2e2").css("color", "grey");
+});
+$("#e2e2").mouseout(function(){
+$("#e2e2").css("color", "black");
+});
+
+$("#e2e1").hover(function(){
+$("#e2e1").css("color", "grey");
+});
+$("#e2e1").mouseout(function(){
+$("#e2e1").css("color", "black");
+});
+
+$("#e1").hover(function(){
+$("#e1").css("color", "grey");
+});
+$("#e1").mouseout(function(){
+$("#e1").css("color", "black");
+});
+
+// alert("[whatever-little-message-you-want]");\\
+
+$("#a4").hover(function(){
+$("#a4").css("color", "grey");
+});
+$("#a4").mouseout(function(){
+$("#a4").css("color", "black");
+});
+
+$("#a3").hover(function(){
+$("#a3").css("color", "grey");
+});
+$("#a3").mouseout(function(){
+$("#a3").css("color", "black");
+});
+
+$("#a2").hover(function(){
+$("#a2").css("color", "grey");
+});
+$("#a2").mouseout(function(){
+$("#a2").css("color", "black");
+});
+
+$("#a1").hover(function(){
+$("#a1").css("color", "grey");
+});
+$("#a1").mouseout(function(){
+$("#a1").css("color", "black");
+});
+
+// alert("[whatever-little-message-you-want]");
+
+$("#lab").hover(function(){
+$("#lab").css("color", "grey");
+});
+$("#lab").mouseout(function(){
+$("#lab").css("color", "white");
+});
+
+$("#studio2").hover(function(){
+$("#studio2").css("color", "grey");
+});
+$("#studio2").mouseout(function(){
+$("#studio2").css("color", "white");
+});
+
+$("#studio1").hover(function(){
+$("#studio1").css("color", "grey");
+});
+$("#studio1").mouseout(function(){
+$("#studio1").css("color", "white");
+});
+
+// alert("[whatever-little-message-you-want]");
+
+$("#studio1").click(function(){
+$(".studio1").show();
+});
+$("#studio1").click(function(){
+$(".studio2").hide();
+});
+$("#studio1").click(function(){
+$(".lab").hide();
+});
+
+$("#studio2").click(function(){
+$(".studio2").show();
+});
+$("#studio2").click(function(){
+$(".studio1").hide();
+});
+$("#studio2").click(function(){
+$(".lab").hide();
+});
+
+$("#lab").click(function(){
+$(".lab").show();
+});
+$("#lab").click(function(){
+$(".studio1").hide();
+});
+$("#lab").click(function(){
+$(".studio2").hide();
+});
+
+
+    })
