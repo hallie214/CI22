dd9b109773e0700969548bf55edbadc8fb8ee108
hallie214 <younh524@newschool.edu>
2022-04-08 00:13:31 -0400

    Assignment 5

---
+++ studio/activity1/a1.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+// alert("[whatever-little-message-you-want]");
+
+$("#title").hover(function(){
+$("#title").css("color", "grey");
+});
+$("#title").mouseout(function(){
+$("#title").css("color", "black");
+});
+
+})
